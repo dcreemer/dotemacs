3ce701e64b344fcbd5c0c50c3c75f1a4a41918c2
David Creemer <dcreemer@flipboard.com>
2014-11-17 14:50:15 -0800

    add silver searcher

--- .emacs.d/lisp/init-ack.el
+++ .emacs.d/lisp/init-ack.el
@@ -12,6 +12,18 @@
 (setq-default grep-highlight-matches t
               grep-scroll-output t)
 
+;; silver searcher as a replacement for ack/grep
+(require-package 'ag)
+(setq ag-reuse-buffers t
+      ag-highlight-search t)
+
+(after-load 'ag
+  (add-to-list 'ag-arguments "--hidden"))
+
+(add-hook 'ag-mode-hook
+          (lambda ()
+            (copy-face 'lazy-highlight 'ag-match-face)))
+
 (when *is-a-mac*
   (setq-default locate-command "mdfind"))
 
