aad8f30e8288913fdf95166e0980b25e1673cc37
David Creemer <dcreemer@flipboard.com>
2014-11-17 14:52:48 -0800

    tweak clojure/cider. still needs work

--- .emacs.d/lisp/init-clojure.el
+++ .emacs.d/lisp/init-clojure.el
@@ -7,14 +7,24 @@
 (require-package 'cider)
 (require-package 'clojure-mode)
 (require-package 'clj-refactor)
-(add-hook 'clojure-mode-hook (lambda ()
-                               (clj-refactor-mode 1)
-                               (cljr-add-keybindings-with-prefix "C-c C-m")
-                               (diminish 'clj-refactor-mode)
-                               (subword-mode 1)
-                               (prettify-symbols-mode)))
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-(add-hook 'cider-repl-mode-hook 'subword-mode)
+
+(after-load 'cider-mode
+  (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+  (add-hook 'cider-mode-hook 'company-mode)
+  (add-hook 'cider-repl-mode-hook 'company-mode)
+  (add-hook 'cider-repl-mode-hook 'subword-mode)
+  (add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
+  (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode))
+
+(after-load 'clojure-mode
+  (add-hook 'clojure-mode-hook (lambda ()
+                                 (clj-refactor-mode 1)
+                                 (cljr-add-keybindings-with-prefix "C-c C-m")
+                                 (diminish 'clj-refactor-mode)
+                                 (prettify-symbols-mode)
+                                 (clojure-enable-cider)
+                                 (diminish 'eldoc-mode))))
+
 
 (provide 'init-clojure)
 
