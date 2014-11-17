d7391cd8389cc1588ad4f2a1dd3068851a07fecd
David Creemer <dcreemer@flipboard.com>
2014-11-17 14:51:53 -0800

    add anaconda. favor flake8

--- .emacs.d/lisp/init-python.el
+++ .emacs.d/lisp/init-python.el
@@ -6,18 +6,22 @@
 
 (require-package 'python-mode)
 (require-package 'pip-requirements)
-(require-package 'py-autopep8)
 (require-package 'anaconda-mode)
 (require-package 'company-anaconda)
 
 (add-auto-mode 'python-mode "\\.py$")
-(setq py-electric-colon-active t
-      py-autopep8-options '("--max-line-length=100"))
+
+(require 'python-mode)
+(setq py-electric-colon-active t)
 
 (require 'company)
 (add-to-list 'company-backends 'company-anaconda)
-(add-hook 'python-mode-hook 'anaconda-mode)
-(add-hook 'python-mode-hook 'eldoc-mode)
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (anaconda-mode 1)
+            (eldoc-mode 1)
+            (setq flycheck-flake8-maximum-line-length 100)))
 
 (require 'anaconda-mode)
 (define-key anaconda-mode-map (kbd "M-,") 'anaconda-nav-pop-marker)
