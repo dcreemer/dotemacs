5a631b20ba59b685257a71db3c9c57e124a48958
David Creemer <dcreemer@flipboard.com>
2014-11-19 15:06:11 -0800

    don't flycheck all the time

--- .emacs.d/lisp/init-flycheck.el
+++ .emacs.d/lisp/init-flycheck.el
@@ -9,7 +9,7 @@
 
   ;; Override default flycheck triggers
   (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
-        flycheck-idle-change-delay 0.8
+        flycheck-idle-change-delay 3.0
         flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
 
 (provide 'init-flycheck)
