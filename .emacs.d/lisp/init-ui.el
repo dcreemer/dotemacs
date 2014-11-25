d65265dc998555977f83fd34809b153ffd5fcaa8
David Creemer <dcreemer@flipboard.com>
2014-11-25 14:31:36 -0800

    tweak font

--- .emacs.d/lisp/init-ui.el
+++ .emacs.d/lisp/init-ui.el
@@ -32,7 +32,7 @@
 ;; DejaVu Sans Mono
 (when (and (display-graphic-p)
            (find-font (font-spec :name "DejaVu Sans Mono")))
-  (set-frame-font "DejaVu Sans Mono-10" t t))
+  (set-frame-font "DejaVu Sans Mono-12" t t))
 
 ;; set frame title to full path of file:
 (when (display-graphic-p)
