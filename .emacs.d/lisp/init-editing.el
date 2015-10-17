bef87db343611605f7dc13de751377f80c9021f9
David Creemer <dcreemer@flipboard.com>
2015-10-17 16:29:48 -0700

    simplify binding

--- .emacs.d/lisp/init-editing.el
+++ .emacs.d/lisp/init-editing.el
@@ -60,7 +60,7 @@
 
 ;; multiple-cursors
 (require-package 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C-c m") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
