d4413688623466421facd5ed88429c38f4fbc89d
David Creemer <dcreemer@flipboard.com>
2015-10-17 16:32:43 -0700

    no yas in markdown mode

--- .emacs.d/lisp/init-markdown.el
+++ .emacs.d/lisp/init-markdown.el
@@ -8,7 +8,6 @@
 
 (add-auto-mode 'markdown-mode "\\.md$" "\\.markdown$")
 
-(add-hook 'markdown-mode-hook 'yas-minor-mode)
 (add-hook 'markdown-mode-hook 'auto-fill-mode)
 (add-hook 'markdown-mode-hook 'fci-mode)
 
