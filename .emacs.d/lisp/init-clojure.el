cfcb7cffb70cbe74366e7c90d49526d88978b2bb
David Creemer <dcreemer@flipboard.com>
2015-10-17 16:33:56 -0700

    tweak clojure modes

--- .emacs.d/lisp/init-clojure.el
+++ .emacs.d/lisp/init-clojure.el
@@ -9,22 +9,18 @@
 ;; (require-package 'clj-refactor)
 
 (after-load 'cider-mode
-  (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-  (add-hook 'cider-mode-hook 'company-mode)
-  (add-hook 'cider-repl-mode-hook 'company-mode)
-  (add-hook 'cider-repl-mode-hook 'subword-mode)
-  (add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
-  (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode))
+  (setq nrepl-log-messages t)
+  (add-hook 'cider-mode-hook #'cider-turn-on-eldoc-mode)
+  (add-hook 'cider-mode-hook #'company-mode)
+  (add-hook 'cider-repl-mode-hook #'company-mode)
+  (add-hook 'cider-repl-mode-hook #'subword-mode)
+  (add-hook 'cider-repl-mode-hook #'smartparens-strict-mode)
+  (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
+  (add-hook 'clojure-mode-hook #'clojure-enable-cider))
 
 (after-load 'clojure-mode
-  (add-hook 'clojure-mode-hook (lambda ()
-                                 ;; (clj-refactor-mode 1)
-                                 ;; (cljr-add-keybindings-with-prefix "C-c C-m")
-                                 ;; (diminish 'clj-refactor-mode)
-                                 (prettify-symbols-mode)
-                                 (clojure-enable-cider)
-                                 (diminish 'eldoc-mode))))
-
+  (add-hook 'clojure-mode-hook 'prettify-symbols-mode)
+  (add-hook 'clojure-mode-hook (lambda () (diminish 'eldoc-mode))))
 
 (provide 'init-clojure)
 
