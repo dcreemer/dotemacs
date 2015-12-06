b8ef0083e1436aad4c9242bf9fcd5a2bfc1cd4bc
David Creemer <dcreemer@flipboard.com>
2015-12-06 11:16:16 -0800

    minor org presentation cleanup

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -13,7 +13,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(italic ((t (:slant italic))))
+ '(org-document-title ((t (:foreground "#4c83ff" :height 1.0))))
  '(org-level-1 ((t (:foreground "#ff1493" :height 1.0))))
  '(org-level-2 ((t (:foreground "#ffff00" :height 1.0))))
  '(org-level-3 ((t (:foreground "#4c83ff" :height 1.0))))
- '(org-document-title ((t (:foreground "#4c83ff" :height 1.0)))))
+ '(underline ((t (:underline t)))))
