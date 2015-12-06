86c38af433942717274364c0a74d6698eba45c2d
David Creemer <dcreemer@flipboard.com>
2015-12-06 12:00:05 -0800

    remove unused hydras

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -269,39 +269,6 @@
   (which-key-setup-side-window-right))
 
 (use-package hydra)
-(defhydra hydra-window (:hint nil)
-  "
-          Split: _v_ert  _s_:horz
-         Delete: _c_lose  _o_nly
-  Switch Window: _h_:left  _j_:down  _k_:up  _l_:right
-        Buffers: _p_revious  _n_ext  _b_:select  _f_ind-file  _F_projectile
-         Winner: _u_ndo  _r_edo
-         Resize: _H_:splitter left  _J_:splitter down  _K_:splitter up  _L_:splitter right
-           Move: _a_:up  _z_:down  _i_menu"
-  ("z" scroll-up-line)
-  ("a" scroll-down-line)
-  ("i" idomenu)
-  ("u" winner-undo)
-  ("r" winner-redo)
-  ("h" windmove-left)
-  ("j" windmove-down)
-  ("k" windmove-up)
-  ("l" windmove-right)
-  ("p" previous-buffer)
-  ("n" next-buffer)
-  ("b" ido-switch-buffer)
-  ("f" ido-find-file)
-  ("F" projectile-find-file)
-  ("s" split-window-below)
-  ("v" split-window-right)
-  ("c" delete-window)
-  ("o" delete-other-windows)
-  ("H" hydra-move-splitter-left)
-  ("J" hydra-move-splitter-down)
-  ("K" hydra-move-splitter-up)
-  ("L" hydra-move-splitter-right)
-  ("q" nil))
-(global-set-key (kbd "C-c w") #'hydra-window/body)
 
 ;; spelling
 (use-package ispell
@@ -824,42 +791,6 @@
   (projectile-global-mode)
   (define-key projectile-mode-map [remap projectile-grep] 'projectile-ag))
 
-(defhydra hydra-projectile (:color teal :hint nil)
-  "
-     PROJECTILE: %(projectile-project-root)
-
-     Find File            Search/Tags          Buffers                Cache
-------------------------------------------------------------------------------------------
-_s-f_: file            _a_: ag                _i_: Ibuffer           _c_: cache clear
- _ff_: file dwim       _g_: update gtags      _b_: switch to buffer  _x_: remove known project
- _fd_: file curr dir   _o_: multi-occur     _s-k_: Kill all buffers  _X_: cleanup non-existing
-  _r_: recent file                                               ^^^^_z_: cache current
-  _d_: dir
-
-"
-  ("a"   projectile-ag)
-  ("b"   projectile-switch-to-buffer)
-  ("c"   projectile-invalidate-cache)
-  ("d"   projectile-find-dir)
-  ("s-f" projectile-find-file)
-  ("ff"  projectile-find-file-dwim)
-  ("fd"  projectile-find-file-in-directory)
-  ("g"   ggtags-update-tags)
-  ("s-g" ggtags-update-tags)
-  ("i"   projectile-ibuffer)
-  ("K"   projectile-kill-buffers)
-  ("s-k" projectile-kill-buffers)
-  ("m"   projectile-multi-occur)
-  ("o"   projectile-multi-occur)
-  ("s-p" projectile-switch-project "switch project")
-  ("p"   projectile-switch-project)
-  ("s"   projectile-switch-project)
-  ("r"   projectile-recentf)
-  ("x"   projectile-remove-known-project)
-  ("X"   projectile-cleanup-known-projects)
-  ("z"   projectile-cache-current-file)
-  ("`"   hydra-projectile-other-window/body "other window")
-  ("q"   nil "cancel" :color blue))
 
 ;; -----------------------------------------------------------------------------
 ;; Major editing modes
