49db1097211ef40ac941e0b2549af7a60fb06100
David Creemer <dcreemer@flipboard.com>
2016-08-10 09:20:19 -0700

    checkpoint packages

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,6 +6,9 @@
  '(custom-safe-themes
    (quote
     ("f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "968d1ad07c38d02d2e5debffc5638332696ac41af7974ade6f95841359ed73e3" default)))
+ '(package-selected-packages
+   (quote
+    (yaml-mode which-key w3m use-package twittering-mode todotxt tidy swiper sql-indent smartparens shorten scala-mode restclient request-deferred regex-tool ranger rainbow-delimiters python-mode py-autopep8 puml-mode powerline popwin pip-requirements php-mode pass oauth nginx-mode names multiple-cursors multi-term markdown-mode magit-gh-pulls lui lua-mode log4e lcs json-rpc json-mode ipretty indent-guide ibuffer-vc hydra htmlize hl-sexp highlight-symbol helm-swoop helm-projectile helm-descbinds helm-dash helm-ag groovy-mode google-contacts golden-ratio go-eldoc go-direx github-browse-file git-timemachine git-gutter ggtags fullframe flymd flycheck-pos-tip flycheck-clojure fill-column-indicator expand-region exec-path-from-shell es-mode epresent epc elisp-slime-nav dumb-jump dockerfile-mode dired-sort dired+ dictionary deft dash-at-point cyberpunk-theme crux company-go company-anaconda command-log-mode color-theme circe bbdb auth-password-store alchemist ag ace-window ace-jump-mode)))
  '(safe-local-variable-values (quote ((product . MySQL) (product . mysql)))))
 
 (custom-set-faces
