73320f4a2da6c2296d612e1486296814695f23f2
David Creemer <david@zachary.com>
2016-10-12 14:23:35 -0700

    poke through more env vars

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -66,7 +66,8 @@
   :init
   (setq exec-path-from-shell-check-startup-files nil)
   :config
-  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "GOPATH"))
+  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"
+                 "GOPATH" "OS" "DIST"))
     (add-to-list 'exec-path-from-shell-variables var))
   (exec-path-from-shell-initialize))
 
